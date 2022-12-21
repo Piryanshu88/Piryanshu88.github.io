eafca2770efd45a2f093d2da5415f536ca7a6a9b
Piryanshu88 <deepubisht2004@gmail.com>
2022-12-21 19:09:32 +0530

    adding fav-icon

--- src/Components/Header/HeaderSocial.jsx
+++ src/Components/Header/HeaderSocial.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import "./Header.css"
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { SiHackerrank } from "react-icons/si";
+import "./Header.css";
 export const HeaderSocial = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/piryanshu-bisht-ba1a22231/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/Piryanshu88" target="_blank"><FaGithub/></a>
+      <a
+        href="https://www.linkedin.com/in/piryanshu-bisht-ba1a22231/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/Piryanshu88" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://www.hackerrank.com/deepubisht2004" target="_blank">
+        <SiHackerrank />
+      </a>
     </div>
-  )
-  }
+  );
+};
