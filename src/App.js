dbb3539c540d563cc4e06da36534221295c17b22
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-27 22:36:02 +0530

    adding responsiveness in navbar

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       {" "}
-      {!ready ? (
+      {!true ? (
         <>
           <Loading />
         </>
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Text,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 import styles from "./Navbar.module.css";
 export const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
   return (
     <div>
       <Flex
@@ -74,6 +92,94 @@ export const Navbar = () => {
           >
             Resume
           </Text>
+        </div>
+        {/* Navbar for mobile screens */}
+        <div div className={styles.mobile_nav}>
+          <Button
+            ref={btnRef}
+            onClick={onOpen}
+            as={IconButton}
+            colorScheme=""
+            icon={<HamburgerIcon />}
+            variant="outline"
+          >
+            Open
+          </Button>
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            finalFocusRef={btnRef}
+          >
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerBody>
+                <VStack lineHeight={"2rem"} marginTop="50px">
+                  <Text
+                    fontSize={"xl"}
+                    cursor="pointer"
+                    _hover={{
+                      color: "var(--color-primary)",
+                      borderBottom: "2px solid var(--color-primary)",
+                    }}
+                  >
+                    Home
+                  </Text>
+                  <Text
+                    fontSize={"xl"}
+                    cursor="pointer"
+                    _hover={{
+                      color: "var(--color-primary)",
+                      borderBottom: "2px solid var(--color-primary)",
+                    }}
+                  >
+                    About
+                  </Text>
+                  <Text
+                    fontSize={"xl"}
+                    cursor="pointer"
+                    _hover={{
+                      color: "var(--color-primary)",
+                      borderBottom: "2px solid var(--color-primary)",
+                    }}
+                  >
+                    Skills
+                  </Text>
+                  <Text
+                    fontSize={"xl"}
+                    cursor="pointer"
+                    _hover={{
+                      color: "var(--color-primary)",
+                      borderBottom: "2px solid var(--color-primary)",
+                    }}
+                  >
+                    Projects
+                  </Text>
+                  <Text
+                    fontSize={"xl"}
+                    cursor="pointer"
+                    _hover={{
+                      color: "var(--color-primary)",
+                      borderBottom: "2px solid var(--color-primary)",
+                    }}
+                  >
+                    Contact
+                  </Text>
+                  <Text
+                    fontSize={"xl"}
+                    cursor="pointer"
+                    _hover={{
+                      color: "var(--color-primary)",
+                      borderBottom: "2px solid var(--color-primary)",
+                    }}
+                  >
+                    Resume
+                  </Text>
+                </VStack>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
         </div>
       </Flex>
     </div>
